adc60626f5a139b6ecf3a44cb2532ed4e1383468
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-08-12 13:42:34 -0500

    chore: setup basic spring project structure

---
+++ src/main/java/com/msdp/cps_system/CpsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.msdp.cps_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpsSystemApplication.class, args);
+	}
+
+}
