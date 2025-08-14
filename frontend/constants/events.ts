39f26ac5e8d9d613026d008d86f3b593ad5027dd
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-08-14 16:19:26 -0500

    feat: add events and urls constants

---
+++ frontend/constants/events.ts
@@ -0,0 +1,6 @@
+export const EVENT_OPTIONS = [
+  { value: "", label: "Select an event" },
+  { value: "equipment_failure", label: "Equipment Failure" },
+  { value: "intense_sunlight_impact", label: "Intense Sunlight Impact" },
+  { value: "sudden_cloud_cover", label: "Sudden Cloud Cover" },
+];
---
+++ frontend/constants/urls.ts
@@ -0,0 +1,7 @@
+const API_BASE_URL=process.env.NEXT_PUBLIC_API_BASE_URL
+
+export const BACKEND_URLS={
+    INTENSE_SUNLIGHT_IMPACT: API_BASE_URL + "/api/v1/weather-events/intense-sunlight-impact",
+    SUDDEN_CLOUD_COVER: API_BASE_URL + "/api/v1/weather-events/cloud-cover-impact",
+    EQUIPMENT_FAILURE: API_BASE_URL + "/api/v1/equipment-events/failure-impact"
+};
